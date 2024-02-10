99aa6a27d2869d211e82f33331d2bda1a54bc4ba
psyark <psyark@gmail.com>
2024-02-10 12:59:53 +0900

    TableInf, ClassInf

--- estat.go
+++ estat.go
@@ -2,6 +2,7 @@ package estat
 
 import (
 	"encoding/json"
+	"time"
 )
 
 // https://www.e-stat.go.jp/api/api-info/e-stat-manual3-0
@@ -10,11 +11,21 @@ type Response struct {
 }
 
 type GetStatsData struct {
+	Result          Result          `json:"RESULT"`
 	Parameter       Parameter       `json:"PARAMETER"`
-	Result          json.RawMessage `json:"RESULT"`
 	StatisticalData StatisticalData `json:"STATISTICAL_DATA"`
 }
 
+// Result is 4.1.1. RESULT タグ
+// すべてのAPI共通で、以下の要素を出力します。
+type Result struct {
+	Date     time.Time `json:"DATE"`
+	ErrorMsg string    `json:"ERROR_MSG"`
+	Status   int       `json:"STATUS"`
+}
+
+// Parameter is 4.4.1. PARAMETER タグ
+// リクエスト時に指定されたパラメータを出力します。パラメータ名を間違えた場合や別のAPIのパラメータを指定した場合は出力されません。
 type Parameter struct {
 	AnnotationGetFlg  string `json:"ANNOTATION_GET_FLG"`
 	CntGetFlg         string `json:"CNT_GET_FLG"`
@@ -28,10 +39,12 @@ type Parameter struct {
 	StatsDataID       string `json:"STATS_DATA_ID"`
 }
 
+// StatisticalData is 4.4.2. STATISTICAL_DATA タグ
+// 統計データの情報を出力します。エラーがあった場合はこのタグ自体出力されません。
 type StatisticalData struct {
 	ResultInf ResultInf       `json:"RESULT_INF"`
 	TableInf  TableInf        `json:"TABLE_INF"`
-	ClassInf  json.RawMessage `json:"CLASS_INF"`
+	ClassInf  ClassInf        `json:"CLASS_INF"`
 	DataInf   json.RawMessage `json:"DATA_INF"`
 }
 
@@ -42,31 +55,47 @@ type ResultInf struct {
 }
 
 type TableInf struct {
-	ID                   string                `json:"@id"`
-	COLLECT_AREA         string                `json:"COLLECT_AREA"`
-	CYCLE                string                `json:"CYCLE"`
-	DESCRIPTION          string                `json:"DESCRIPTION"`
-	GOV_ORG              CodeWithDescription   `json:"GOV_ORG"`
-	MAIN_CATEGORY        CodeWithDescription   `json:"MAIN_CATEGORY"`
-	OPEN_DATE            string                `json:"OPEN_DATE"`
-	OVERALL_TOTAL_NUMBER int                   `json:"OVERALL_TOTAL_NUMBER"`
-	SMALL_AREA           int                   `json:"SMALL_AREA"`
-	STATISTICS_NAME      string                `json:"STATISTICS_NAME"`
-	STATISTICS_NAME_SPEC json.RawMessage       `json:"STATISTICS_NAME_SPEC"`
-	STAT_NAME            CodeWithDescription   `json:"STAT_NAME"`
-	SUB_CATEGORY         CodeWithDescription   `json:"SUB_CATEGORY"`
-	SURVEY_DATE          json.RawMessage       `json:"SURVEY_DATE"`
-	TITLE                NumberWithDescription `json:"TITLE"`
-	TITLE_SPEC           json.RawMessage       `json:"TITLE_SPEC"`
-	UPDATED_DATE         string                `json:"UPDATED_DATE"`
+	ID                   string          `json:"@id"`
+	COLLECT_AREA         string          `json:"COLLECT_AREA"`
+	CYCLE                string          `json:"CYCLE"`
+	DESCRIPTION          string          `json:"DESCRIPTION"`
+	GOV_ORG              AnnotatedCode   `json:"GOV_ORG"`
+	MAIN_CATEGORY        AnnotatedCode   `json:"MAIN_CATEGORY"`
+	OPEN_DATE            string          `json:"OPEN_DATE"`
+	OVERALL_TOTAL_NUMBER int             `json:"OVERALL_TOTAL_NUMBER"`
+	SMALL_AREA           int             `json:"SMALL_AREA"`
+	STATISTICS_NAME      string          `json:"STATISTICS_NAME"`
+	STATISTICS_NAME_SPEC json.RawMessage `json:"STATISTICS_NAME_SPEC"`
+	STAT_NAME            AnnotatedCode   `json:"STAT_NAME"`
+	SUB_CATEGORY         AnnotatedCode   `json:"SUB_CATEGORY"`
+	SURVEY_DATE          json.RawMessage `json:"SURVEY_DATE"`
+	TITLE                AnnotatedNo     `json:"TITLE"`
+	TITLE_SPEC           json.RawMessage `json:"TITLE_SPEC"`
+	UPDATED_DATE         string          `json:"UPDATED_DATE"`
 }
 
-type CodeWithDescription struct {
-	Code        string `json:"@code"`
-	Description string `json:"$"`
+type ClassInf struct {
+	ClassObj []ClassObj `json:"CLASS_OBJ"`
+}
+type ClassObj struct {
+	ID    string  `json:"@id"`
+	Name  string  `json:"@name"`
+	Class []Class `json:"CLASS"`
+}
+type Class struct {
+	Code       string `json:"@code"`
+	Level      string `json:"@level"`
+	Name       string `json:"@name"`
+	Unit       string `json:"@unit,omitempty"`
+	ParentCode string `json:"@parentCode,omitempty"`
 }
 
-type NumberWithDescription struct {
-	No          string `json:"@no"`
-	Description string `json:"$"`
+type AnnotatedCode struct {
+	Code       string `json:"@code"`
+	Annotation string `json:"$"`
+}
+
+type AnnotatedNo struct {
+	No         string `json:"@no"`
+	Annotation string `json:"$"`
 }
