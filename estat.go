069e380116b1932fee101d9608e84ff7fe562363
psyark <psyark@gmail.com>
2024-02-12 11:31:38 +0900

    テスト対象追加

--- estat.go
+++ estat.go
@@ -92,7 +92,7 @@ type TableInf struct {
 	STAT_NAME            AnnotatedCode   `json:"STAT_NAME"`
 	SUB_CATEGORY         AnnotatedCode   `json:"SUB_CATEGORY"`
 	SURVEY_DATE          json.RawMessage `json:"SURVEY_DATE"`
-	TITLE                AnnotatedNo     `json:"TITLE"`
+	TITLE                json.RawMessage `json:"TITLE"`
 	TITLE_SPEC           json.RawMessage `json:"TITLE_SPEC"`
 	UPDATED_DATE         string          `json:"UPDATED_DATE"`
 }
@@ -101,10 +101,30 @@ type ClassInf struct {
 	ClassObj []ClassObj `json:"CLASS_OBJ"`
 }
 type ClassObj struct {
-	ID    string  `json:"@id"`
-	Name  string  `json:"@name"`
-	Class []Class `json:"CLASS"`
+	ID    string      `json:"@id"`
+	Name  string      `json:"@name"`
+	Class ClassHelper `json:"CLASS"`
 }
+
+type ClassHelper []Class
+
+func (c *ClassHelper) UnmarshalJSON(d []byte) error {
+	if d[0] == '{' {
+		*c = []Class{{}}
+		return json.Unmarshal(d, &(*c)[0])
+	} else {
+		return json.Unmarshal(d, (*[]Class)(c))
+	}
+}
+
+func (c ClassHelper) MarshalJSON() ([]byte, error) {
+	if len(c) == 1 {
+		return json.Marshal(c[0])
+	} else {
+		return json.Marshal([]Class(c))
+	}
+}
+
 type Class struct {
 	Code       string `json:"@code"`
 	Level      string `json:"@level"`
@@ -117,8 +137,27 @@ type Class struct {
 // 指定した絞り込み条件又はデータセットの条件又はその両方の条件によって抽出されるデータ件数が 0 の場合、このタグは出力されません。
 // また、件数取得フラグ(cntGetFlg)に”Y”(件数のみ取得する)を指定した場合も出力されません。
 type DataInf struct {
-	Note  []Note  `json:"NOTE"`
-	Value []Value `json:"VALUE"`
+	Note  NoteHelper `json:"NOTE"`
+	Value []Value    `json:"VALUE"`
+}
+
+type NoteHelper []Note
+
+func (c *NoteHelper) UnmarshalJSON(d []byte) error {
+	if d[0] == '{' {
+		*c = []Note{{}}
+		return json.Unmarshal(d, &(*c)[0])
+	} else {
+		return json.Unmarshal(d, (*[]Note)(c))
+	}
+}
+
+func (c NoteHelper) MarshalJSON() ([]byte, error) {
+	if len(c) == 1 {
+		return json.Marshal(c[0])
+	} else {
+		return json.Marshal([]Note(c))
+	}
 }
 
 type Note struct {
@@ -129,9 +168,11 @@ type Note struct {
 // VALUE	統計数値(セル)の情報です。データ件数分だけ出力されます。
 // 属性として表章事項コード(tab)、分類事項コード(cat01 ～ cat15)、地域事項コード(area)、時間軸事項コード(time)、単位(unit)、注釈記号(anotation)を保持します。全ての属性はデータがある場合のみ出力されます。
 type Value struct {
-	Value int    `json:"$,string"`
-	Cat01 string `json:"@cat01"`
-	Tab   string `json:"@tab"`
+	Value any    `json:"$"`
+	Cat01 string `json:"@cat01,omitempty"`
+	Cat02 string `json:"@cat02,omitempty"`
+	Cat03 string `json:"@cat03,omitempty"`
+	Tab   string `json:"@tab,omitempty"`
 	Time  string `json:"@time"`
 	Unit  string `json:"@unit"`
 }
--- test/estat_test.go
+++ test/estat_test.go
@@ -19,47 +19,74 @@ func TestXxx(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	query, err := url.ParseQuery("lang=J&statsDataId=0004009602&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0")
-	if err != nil {
-		t.Fatal(err)
-	}
+	t.Run("0003354197", r("lang=J&statsDataId=0003354197&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"))
+	t.Run("0004009602", r("lang=J&statsDataId=0004009602&metaGetFlg=Y&cntGetFlg=N&explanationGetFlg=Y&annotationGetFlg=Y&sectionHeaderFlg=1&replaceSpChars=0"))
+}
 
-	query.Set("appId", os.Getenv("appId"))
+func r(query string) func(*testing.T) {
+	return func(t *testing.T) {
+		query, err := url.ParseQuery(query)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	resp, err := http.Get("http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?" + query.Encode())
-	if err != nil {
-		t.Fatal(err)
-	}
+		query.Set("appId", os.Getenv("appId"))
 
-	defer resp.Body.Close()
+		resp, err := http.Get("http://api.e-stat.go.jp/rest/3.0/app/json/getStatsData?" + query.Encode())
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		t.Fatal(err)
-	}
+		defer resp.Body.Close()
 
-	untyped := map[string]any{}
-	if err := json.Unmarshal(data, &untyped); err != nil {
-		t.Fatal(err)
-	}
+		data, err := io.ReadAll(resp.Body)
+		if err != nil {
+			t.Fatal(err)
+		}
 
-	typed := estat.Response{}
-	if err := json.Unmarshal(data, &typed); err != nil {
-		t.Fatal(err)
-	}
+		untyped := map[string]any{}
+		if err := json.Unmarshal(data, &untyped); err != nil {
+			t.Fatal(err)
+		}
 
-	data1, _ := json.MarshalIndent(untyped, "", "  ")
-	data2, _ := json.MarshalIndent(typed, "", "  ")
-	patch, err := jsondiff.CompareJSON(data2, data1)
-	if err != nil {
-		t.Fatal(err)
-	}
+		typed := estat.Response{}
+		if err := json.Unmarshal(data, &typed); err != nil {
+			t.Fatal(err)
+		}
 
-	for _, op := range patch {
-		fmt.Printf("%s\n", op)
-	}
+		data1, _ := json.MarshalIndent(untyped, "", "  ")
+		data2, _ := json.MarshalIndent(typed, "", "  ")
+		{
+			patch, err := jsondiff.CompareJSON(data2, data1)
+			if err != nil {
+				t.Fatal(err)
+			}
 
-	if len(patch) != 0 {
-		t.Fatal("unmatch")
+			for _, op := range patch {
+				fmt.Printf("%s\n", op)
+			}
+		}
+		{
+			patch, err := jsondiff.CompareJSON(data1, data2)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			for _, op := range patch {
+				fmt.Printf("%s\n", op)
+			}
+
+			if len(patch) != 0 {
+				t.Fatal("unmatch")
+			}
+		}
+
+		// fmt.Println(string(data2))
+		for _, classObj := range typed.GetStatsData.StatisticalData.ClassInf.ClassObj {
+			fmt.Println(classObj.ID, classObj.Name)
+			for _, class := range classObj.Class {
+				fmt.Println("    ", class)
+			}
+		}
 	}
 }
